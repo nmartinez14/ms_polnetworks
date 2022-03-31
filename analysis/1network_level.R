76c5be81010deb385120087ec0193358bc2410df
Nicole Martinez Llaurador <91992903+nmartinez14@users.noreply.github.com>
2022-03-30 17:02:05 -0700

    Begin script for analysis

---
+++ analysis/1network_level.R
@@ -0,0 +1,7 @@
+rm(list = ls())
+setwd("C:/Users/na_ma/Dropbox (University of Oregon)/ms_polnetworks")
+library(tidyverse)
+library(bipartite)
+load(file = "data/pol_net.Rdata")
+load(file = "data/plots.csv")
+
